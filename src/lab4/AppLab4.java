03ff30708a4a5deeb785c9ead96722133d873d21
Ana-Maria Balici <anamaria.balici@ulbsibiu.ro>
2026-03-29 18:26:21 +0300

    Laborator 4

--- src/lab4/AppLab4.java
+++ src/lab4/AppLab4.java
@@ -1,10 +1,16 @@
 package lab4;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class AppLab4 {
     static void main() {
+        //problema 4.5.1
         HashMap<String, Integer> varste = new HashMap<>();
         varste.put("Ioan", 21);
         varste.put("Maria", 22);
@@ -14,25 +20,61 @@ public class AppLab4 {
         varste.put("Mihai", 21);
         varste.put("Daniela", 23);
         System.out.println(" ");
-        System.out.println("Varstele initiale->"+varste);
+        System.out.println("Varstele initiale->" + varste);
         varste.put("Vlad", 19);
         varste.put("Iulia", 19);
         System.out.println(" ");
-        System.out.println("Varstele schimbate->"+varste);
+        System.out.println("Varstele schimbate->" + varste);
         Map<String, String> adrese = Map.of("Ioan", "Sibiu", "Maria", "Bucuresti", "Victor",
-                "Cluj","Simina", "Alba-Iulia","Marius", "Medias", "Mihai", "Cisnadie","Daniela", "Sibiu");
-        HashMap<String,Tanar> tineri=new HashMap<>();
-        for(String nume: varste.keySet())
-        {
-            int varsta=varste.get(nume);
-            String adresa=adrese.getOrDefault(nume,"Necunoscuta");
-            Tanar t=new Tanar(nume,varsta,adresa);
-            tineri.put(nume,t);
+                "Cluj", "Simina", "Alba-Iulia", "Marius", "Medias", "Mihai", "Cisnadie", "Daniela", "Sibiu");
+        HashMap<String, Tanar> tineri = new HashMap<>();
+        for (String nume : varste.keySet()) {
+            int varsta = varste.get(nume);
+            String adresa = adrese.getOrDefault(nume, "Necunoscuta");
+            Tanar t = new Tanar(nume, varsta, adresa);
+            tineri.put(nume, t);
         }
         System.out.println(" ");
-        for(String cheie:tineri.keySet())
-        {
-            System.out.println(cheie+"->"+tineri.get(cheie));
+        for (String cheie : tineri.keySet()) {
+            System.out.println(cheie + "->" + tineri.get(cheie));
+        }
+
+        //problema 4.5.2
+        Map<String, Student> mapStudenti = new HashMap<>();
+        try {
+            Path caleFisier = Paths.get("src/lab4/Studentii.txt");
+            List<String> linieStudenti = Files.readAllLines(caleFisier);
+            for (String linieStudent : linieStudenti) {
+                String[] campuriStudent = linieStudent.split(",");
+                String nrmatricol = campuriStudent[0];
+                String prenume = campuriStudent[1];
+                String nume = campuriStudent[2];
+                String formatieDeStudiu = campuriStudent[3];
+                mapStudenti.put(nrmatricol, new Student(nume, prenume, nrmatricol, formatieDeStudiu));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            Path caleNote = Paths.get("src/lab4/note_anon.txt");
+            List<String> linieNote = Files.readAllLines(caleNote);
+
+            for (String linieNota : linieNote) {
+                String[] campuriNota = linieNota.split(",");
+                String nrMatricolNota = campuriNota[0];
+                double notaStudent = Double.parseDouble(campuriNota[1]);
+                Student studentGasit = mapStudenti.get(nrMatricolNota);
+                if (studentGasit != null) {
+                    studentGasit.SetNota(notaStudent);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(" ");
+        for (Student studentCurent : mapStudenti.values()) {
+            System.out.println(studentCurent);
         }
     }
 }
---
+++ src/lab4/Student.java
@@ -0,0 +1,37 @@
+package lab4;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
+
+public class Student {
+    private String nume,prenume,nrmatricol,formatieDeStudiu;
+    private double nota;
+    public Student(String nume, String prenume, String nrmatricol, String formatieDeStudiu)
+    {
+        this.nume=nume;
+        this.prenume=prenume;
+        this.nrmatricol=nrmatricol;
+        this.formatieDeStudiu=formatieDeStudiu;
+        this.nota = 0.0;
+    }
+    public String toString()
+    {
+        return nume + " " + prenume + " " + nrmatricol + " " + formatieDeStudiu+" "+nota;
+    }
+    public String getNume() {return nume;}
+    public String getPrenume() {return prenume;}
+    public double getNota() {return nota;}
+    public String getNrMatricol() {return nrmatricol;}
+    public void SetNota(double nota) {this.nota = nota;}
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(nrmatricol, student.nrmatricol);
+    }
+    public int hashCode() {
+        return Objects.hash(nrmatricol);
+    }
+}
