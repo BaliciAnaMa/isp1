b546e80349386379db55f2359be283e469622818
Ana-Maria Balici <anamaria.balici@ulbsibiu.ro>
2026-04-06 21:28:58 +0300

    Tema Problema3 Lab5

---
+++ src/lab5/students/StudentBursieri.java
@@ -0,0 +1,32 @@
+package lab5.students;
+
+import java.util.Objects;
+
+public class StudentBursieri extends Student {
+    private double cuantumBursa;
+    public StudentBursieri(int id, String prenume, String nume, String grupa, double medie, double bursa) {
+        this.nrmatricol = "" + id;
+        this.prenume = prenume;
+        this.nume = nume;
+        this.formatieDeStudiu = grupa;
+        this.SetNota(medie);
+        this.cuantumBursa = bursa;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StudentBursieri student = (StudentBursieri) o;
+        return Objects.equals(nrmatricol, student.nrmatricol) && this.cuantumBursa == student.cuantumBursa;
+    }
+
+    public int hashCode() {
+        return Objects.hash(nrmatricol,cuantumBursa);
+    }
+    public String toString() {
+        return "ID: " + nrmatricol + ", Nume: " + nume + " " + prenume +
+                ", Grupa: " + formatieDeStudiu + ", Nota: " + getNota() +
+                ", Bursa: " + cuantumBursa;
+    }
+
+}
