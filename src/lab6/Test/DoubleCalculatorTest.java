29b61c9b958429427897a4a7d3eeac88d0213f2e
Ana-Maria Balici <anamaria.balici@ulbsibiu.ro>
2026-04-11 21:29:22 +0100

    Problema 1+2 cu teste noi lab6

--- src/lab6/Test/DoubleCalculatorTest.java
+++ src/lab6/Test/DoubleCalculatorTest.java
@@ -5,23 +5,57 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DoubleCalculatorTest {
     @Test
-    public void testDoublePrecision() {
-        DoubleCalculator calc = new DoubleCalculator();
-        calc.init();
-
-        calc.add(5.5).multiply(2.0);
-
-        assertEquals(11.0, calc.result(), 0.0001);
+    public void testAddPositive() {
+        DoubleCalculator calc = new DoubleCalculator().init();
+        assertEquals(15.5, calc.add(10.0).add(5.5).result(), 0.0001);
     }
 
     @Test
-    public void testInmultireCuZero() {
-        DoubleCalculator calc = new DoubleCalculator();
-        calc.init();
-
-        calc.add(15.5);
-        calc.multiply(0.0);
-
-        assertEquals(0.0, calc.result(), 0.0001);
+    public void testAddNegatives() {
+        DoubleCalculator calc = new DoubleCalculator().init();
+        assertEquals(-15.5, calc.add(-10.0).add(-5.5).result(), 0.0001);
     }
+
+    @Test
+    public void testSubtractPositives() {
+        DoubleCalculator calc = new DoubleCalculator().init();
+        assertEquals(4.5, calc.add(10.0).subtract(5.5).result(), 0.0001);
+    }
+
+    @Test
+    public void testSubtractNegatives() {
+        DoubleCalculator calc = new DoubleCalculator().init();
+        assertEquals(-4.5, calc.add(-10.0).subtract(-5.5).result(), 0.0001);
+    }
+
+    @Test
+    public void testMultiplyPositives() {
+        DoubleCalculator calc = new DoubleCalculator().init();
+        assertEquals(50.0, calc.add(10.0).multiply(5.0).result(), 0.0001);
+    }
+
+    @Test
+    public void testMultiplyNegatives() {
+        DoubleCalculator calc = new DoubleCalculator().init();
+        assertEquals(50.0, calc.add(-10.0).multiply(-5.0).result(), 0.0001);
+    }
+
+    @Test
+    public void testMultiplyBy0() {
+        DoubleCalculator calc = new DoubleCalculator().init();
+        assertEquals(0.0, calc.add(10.0).multiply(0.0).result(), 0.0001);
+    }
+
+    @Test
+    public void testDividePositives() {
+        DoubleCalculator calc = new DoubleCalculator().init();
+        assertEquals(2.0, calc.add(10.0).divide(5.0).result(), 0.0001);
+    }
+
+    @Test
+    public void testDivideNegatives() {
+        DoubleCalculator calc = new DoubleCalculator().init();
+        assertEquals(2.0, calc.add(-10.0).divide(-5.0).result(), 0.0001);
+    }
+
 }
--- src/lab6/Test/NewIntCalculatorTest.java
+++ src/lab6/Test/NewIntCalculatorTest.java
@@ -5,22 +5,59 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NewIntCalculatorTest {
 
-    @Test
-    public void testComplexOperation() {
-        NewIntCalculator calc = new NewIntCalculator();
-        calc.init();
+        @Test
+        public void testAddPositive() {
+            NewIntCalculator calc = new NewIntCalculator().init();
+            assertEquals(15, calc.add(10).add(5).result());
+        }
 
-        calc.add(10).subtract(2).multiply(3);
+        @Test
+        public void testAddNegatives() {
+            NewIntCalculator calc = new NewIntCalculator().init();
+            assertEquals(-15, calc.add(-10).add(-5).result());
+        }
 
-        assertEquals(24, calc.result(), "Rezultatul ar trebui să fie 24");
+        @Test
+        public void testSubtractPositives() {
+            NewIntCalculator calc = new NewIntCalculator().init();
+            assertEquals(5, calc.add(10).subtract(5).result());
+        }
+
+        @Test
+        public void testSubtractNegatives() {
+            NewIntCalculator calc = new NewIntCalculator().init();
+            assertEquals(-5, calc.add(-10).subtract(-5).result());
+        }
+
+        @Test
+        public void testMultiplyPositives() {
+            NewIntCalculator calc = new NewIntCalculator().init();
+            assertEquals(50, calc.add(10).multiply(5).result());
+        }
+
+        @Test
+        public void testMultiplyNegatives() {
+            NewIntCalculator calc = new NewIntCalculator().init();
+            assertEquals(50, calc.add(-10).multiply(-5).result());
+        }
+
+        @Test
+        public void testMultiplyBy0() {
+            NewIntCalculator calc = new NewIntCalculator().init();
+            assertEquals(0, calc.add(10).multiply(0).result());
+        }
+
+        @Test
+        public void testDividePositives() {
+            NewIntCalculator calc = new NewIntCalculator().init();
+            assertEquals(2, calc.add(10).divide(5).result());
+        }
+
+        @Test
+        public void testDivideNegatives() {
+            NewIntCalculator calc = new NewIntCalculator().init();
+            assertEquals(2, calc.add(-10).divide(-5).result());
+        }
     }
 
-    @Test
-    public void testImpartireLaZero() {
-        NewIntCalculator calc = new NewIntCalculator();
-        calc.init();
-        calc.add(10);
 
-        calc.divide(0);
-    }
-}
