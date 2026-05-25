7c9d6c7cb35843fb054718f84062e92c42ccd51c
Bălici Ana-Maria <anamaria.balici@ulbsibiu.ro>
2026-05-25 22:59:04 +0300

    Problema 11.3.2 lab11

---
+++ src/lab11/students/StudentiExport.java
@@ -0,0 +1,8 @@
+package lab11.students;
+
+import java.util.List;
+
+public interface StudentiExport {
+    void doExport(List<Student> studenti);
+
+}
---
+++ src/lab11/students/decorator/ITimeExecution.java
@@ -0,0 +1,9 @@
+package lab11.students.decorator;
+import lab11.students.Student;
+
+import java.util.List;
+
+public interface ITimeExecution {
+    long executionTime(List<Student> studenti);
+
+}
